3fda973cdea6eaef58b224f643c91a6a68dab479
Eric Richards <esholundr@yahoo.com>
2019-12-12 20:38:16 -0700

    added get and post requests, not working correctly yet

--- api/server.js
+++ api/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const carsRouter = require('../data/cars/cars-router.js');
+const carsRouter = require('../cars/cars-router.js');
 
 const server = express();
+
+const db = require('../data/dbConfig');
 
 server.use(helmet());
 server.use(express.json());
 
-server.use('/api/cars', carsRouter);
+server.use('/api/cars', db, carsRouter);
 
 module.exports = server;
---
+++ cars/cars-router.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const knex = require('knex');
+
+const db = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: './data/car-dealer.db3'
+    },
+    useNullAsDefault: true
+});
+
+const router = express.Router();
+
+// POST
+
+router.post('/', (req, res) => {
+    const carData = req.body;
+    db('cars').insert(carData)
+        .then(ids => {
+            db('cars').where({ id: ids[0] })
+                .then(newCarEntry => {
+                    res.status(201).json(newCarEntry);
+                });
+        })
+        .catch(err => {
+            console.log('POST error', err);
+            res.status(500).json({ message: 'Failed to store data' });
+        });
+});
+
+// GET
+
+router.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.status(200).json(cars);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to retrieve cars', err });
+        });
+});
+
+module.exports = router;
